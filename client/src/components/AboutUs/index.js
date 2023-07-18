c5a7b76f512dffdb99462e2efedd448a21dcbd83
radhey-molsoft <himali.baps@gmail.com>
2023-07-17 20:39:17 -0400

    about us

--- client/src/components/AboutUs/index.js
+++ client/src/components/AboutUs/index.js
@@ -4,6 +4,7 @@ const AboutUs = () => {
   return (
     <div>
   <section>
+    <h2>One stop destination for electric cars!</h2>
     <p>
       At VoltWheels, we are dedicated to promoting sustainable transportation and helping people transition
       to cleaner, greener, and more energy-efficient electric vehicles.
@@ -19,45 +20,6 @@ const AboutUs = () => {
     </p>
   </section>
 
-  <section>
-    <h2>For Buyers</h2>
-    <p>
-      If you're looking to purchase an electric vehicle, you've come to the right place. Our platform offers an extensive
-      inventory of new and used electric vehicles from various makes and models, making it easy for you to find the perfect
-      fit for your lifestyle.
-    </p>
-    <ul>
-      <li>Extensive Inventory: Browse through a vast selection of electric vehicles to find the one that meets your needs.</li>
-      <li>Detailed Vehicle Information: Each listing includes comprehensive specifications and features.</li>
-      <li>Transparent Pricing: Enjoy fair and competitive pricing on all our listings.</li>
-      <li>User Reviews: Read genuine user reviews and ratings to gain insights from other electric vehicle owners.</li>
-      <li>Expert Advice: Our team of electric vehicle enthusiasts is ready to assist you with any questions or guidance you may need.</li>
-    </ul>
-  </section>
-
-  <section>
-    <h2>For Sellers</h2>
-    <p>
-      If you're looking to sell your electric vehicle, our platform offers a convenient and efficient way to reach potential
-      buyers and find the right home for your EV.
-    </p>
-    <ul>
-      <li>Easy Listing Process: Create a detailed listing for your electric vehicle with essential information and attractive images effortlessly.</li>
-      <li>Reach a Wider Audience: Benefit from our extensive user base, reaching potential buyers from across the region.</li>
-      <li>Manage Your Listings: Track and manage your listings conveniently from your account dashboard.</li>
-      <li>Quick and Secure Transactions: We facilitate smooth and secure transactions to ensure a hassle-free selling experience.</li>
-    </ul>
-  </section>
-
-  <section>
-    <h2>Why Choose Us?</h2>
-    <ul>
-      <li>Sustainability: We are committed to promoting eco-friendly transportation solutions and reducing the carbon footprint.</li>
-      <li>Trust and Safety: Our platform ensures a secure and trustworthy environment for all users, with privacy and data protection as top priorities.</li>
-      <li>Community: Join a community of electric vehicle enthusiasts, share experiences, and contribute to a cleaner future together.</li>
-      <li>Customer Support: Our dedicated customer support team is always ready to assist you with any inquiries or issues.</li>
-    </ul>
-  </section>
     </div>
   );
 };
