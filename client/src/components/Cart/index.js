9be6e12047feab6510f78c8bbed610104475ba8e
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-07-19 12:23:34 -0400

    commiting working success page

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { useLazyQuery } from '@apollo/client';
-import { QUERY_CHECKOUT } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import './style.css';
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import "./style.css";
 
-const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
@@ -17,15 +17,16 @@ const Cart = () => {
 
   useEffect(() => {
     if (data) {
-      stripePromise.then((res) => {
-        res.redirectToCheckout({ sessionId: data.checkout.session });
-      });
+      window.location.replace("/success");
+      // stripePromise.then((res) => {
+      //   res.redirectToCheckout({ sessionId: data.checkout.session });
+      // });
     }
   }, [data]);
 
   useEffect(() => {
     async function getCart() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       dispatch({ type: ADD_MULTIPLE_TO_CART, cars: [...cart] });
     }
 
@@ -55,10 +56,11 @@ const Cart = () => {
       }
     });
 
-    getCheckout({
-      variables: { cars: carIds },
-    });
-    console.log('click')
+    // getCheckout({
+    //   variables: { cars: carIds },
+    // });
+    // console.log("click");
+    window.location.replace("/success");
   }
 
   if (!state.cartOpen) {
